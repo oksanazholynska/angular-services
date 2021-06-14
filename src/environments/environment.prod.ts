432c41ec732b6f0eb73f1e26e3af536353cd259e
oksanazholynska <oksanochkazholinska@gmail.com>
2021-06-14 16:11:19 +0300

    added build vcersion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,3 @@
 export const environment = {
   production: true,
-  pokeApi: 'https://pokeapi.co/api/v2',
-  unknowUrl: 'something unreal',
-  firebaseConfig: {
-    apiKey: 'AIzaSyBs4lKr89RBU4wIOfjl8jI3u4coqKHLm9o',
-    authDomain: 'oksanazholynska-59ccb.firebaseapp.com',
-    databaseURL: 'https://oksanazholynska-59ccb-default-rtdb.firebaseio.com',
-    projectId: 'oksanazholynska-59ccb',
-    storageBucket: 'oksanazholynska-59ccb.appspot.com',
-    messagingSenderId: '697463777404',
-    appId: '1:697463777404:web:d5c9c4fba69d3d624a01f9',
-    measurementId: 'G-L0MTLEVP6S',
-  },
 };
